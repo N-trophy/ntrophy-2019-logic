daad4f82e4718d6ecca34ed6b67aca9ac80d14b6
Jan Horacek <me@apophis.cz>
2019-01-31 13:46:05 +0100

    Add evaluation model.

--- api_server/api_server/models/__init__.py
+++ api_server/api_server/models/__init__.py
@@ -1,2 +1,3 @@
 from .submission import Submission
+from .evaluation import Evaluation
 from .level import Level
---
+++ api_server/api_server/models/evaluation.py
@@ -0,0 +1,21 @@
+from django.db import models
+from django.contrib.auth.models import User
+from datetime import datetime
+
+from api_server.models.level import Level
+
+class Evaluation(models.Model):
+    user = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE
+    )
+    level = models.ForeignKey(
+        Level,
+        on_delete=models.CASCADE
+    )
+    score = models.DecimalField(max_digits=8, decimal_places=2)
+    positions = models.TextField() # Positions of cities, json
+    report = models.TextField() # Anything useful
+    datetime = models.DateTimeField(default=datetime.now)
+
+    unique_together = (('user', 'level'),)
