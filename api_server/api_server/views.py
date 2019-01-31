0b290b35f5d7637a7af34d151dc25be210410d0c
Martin Kurecka <kur3cka.martin@gmail.com>
2019-01-31 15:00:49 +0100

    Dynamic level list

--- api_server/api_server/views.py
+++ api_server/api_server/views.py
@@ -1,14 +1,16 @@
 from django.http import HttpResponse, Http404
 from django.shortcuts import render_to_response
 from django.views import View
-from django.template import loader, RequestContext
+from django.template import loader, RequestContext, Library
 from django.core.exceptions import ObjectDoesNotExist
 from django.http import JsonResponse
+
 from django.contrib.auth.decorators import login_required
 
 import json
+from api_server.models import Level
 
-from api_server.models.level import Level
+register = Library()
 
 @login_required
 def graph(request, *args, **kwargs):
@@ -31,10 +33,32 @@ def index(request, *args, **kwargs):
     }
     return HttpResponse(template.render(context, request))
 
+def level_class(next_level, level_id):
+    if next_level > level_id:
+        return "w3-red"
+    elif next_level == level_id:
+        return "w3-white"
+    else:
+        return "w3-dark-gray"
+
+def level_status(next_level, level):
+    if next_level > level.id:
+        return "Skóre: 3"
+    elif next_level == level.id:
+        return "Otevřeno"
+    else:
+        return "Zatím uzavřeno"
+
 @login_required
 def team(request, *args, **kwargs):
     template = loader.get_template('team.html')
-    context = {}
+    next_level = 3
+    levels = list(map(lambda l: { 'id': l.id, 'status': level_status(next_level, l), 'class': level_class(next_level, l.id) }, Level.objects.order_by('id')))
+
+    context = {
+        'levels': levels,
+        'next_level': next_level
+    }
     return HttpResponse(template.render(context, request))
 
 @login_required
