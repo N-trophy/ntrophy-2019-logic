dc1f48ceefbaddc8457dd4f8e4ca6815e299c3cf
Jan Horacek <me@apophis.cz>
2019-02-01 12:23:48 +0100

    Add evaluation & submission to admin.

--- api_server/api_server/admin.py
+++ api_server/api_server/admin.py
@@ -1,4 +1,8 @@
 from django.contrib import admin
 from api_server.models.level import Level
+from api_server.models.evaluation import Evaluation
+from api_server.models.submission import Submission
 
 admin.site.register(Level)
+admin.site.register(Evaluation)
+admin.site.register(Submission)
