72243c502c291ab7c73261e3530ecfca462b02fc
Jan Horacek <me@apophis.cz>
2019-02-01 12:35:53 +0100

    Add fields in admin site.

--- api_server/api_server/admin.py
+++ api_server/api_server/admin.py
@@ -3,6 +3,20 @@ from api_server.models.level import Level
 from api_server.models.evaluation import Evaluation
 from api_server.models.submission import Submission
 
-admin.site.register(Level)
-admin.site.register(Evaluation)
-admin.site.register(Submission)
+@admin.register(Level)
+class LevelAdmin(admin.ModelAdmin):
+    list_display = ('id', 'score', 'no_stations', 'no_evaluations')
+
+
+@admin.register(Evaluation)
+class EvaluationAdmin(admin.ModelAdmin):
+    list_display = ('user', 'level', 'score', 'datetime')
+    list_filter = ('user', 'level', 'datetime')
+    ordering = ['datetime']
+
+
+@admin.register(Submission)
+class SubmissionAdmin(admin.ModelAdmin):
+    list_display = ('user', 'level', 'score', 'datetime')
+    list_filter = ('user', 'level', 'datetime')
+    ordering = ['datetime']
