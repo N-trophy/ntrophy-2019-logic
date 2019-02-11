b000e46d5c4240117b575683bbf4f22ce11f09ec
Jan Horacek <me@apophis.cz>
2019-02-11 17:42:51 +0100

    Add no evaluations & no submission to level table.

--- api_server/api_server/admin.py
+++ api_server/api_server/admin.py
@@ -1,9 +1,18 @@
 from django.contrib import admin
 from api_server.models import Level, Evaluation, Submission, Post
 
+
 @admin.register(Level)
 class LevelAdmin(admin.ModelAdmin):
-    list_display = ('id', 'score', 'no_stations', 'no_evaluations')
+    list_display = ('id', 'score', 'no_stations', 'no_evaluations',
+                    'no_submitted_teams', 'no_evaluated_teams')
+
+    def no_submitted_teams(slf, obj):
+        return Submission.objects.filter(level=obj.id).count()
+
+    def no_evaluated_teams(slf, obj):
+        return Evaluation.objects.filter(level=obj.id).values_list('user').\
+               distinct().count()
 
 
 @admin.register(Evaluation)
@@ -18,6 +27,7 @@ class SubmissionAdmin(admin.ModelAdmin):
     list_display = ('user', 'level', 'score', 'datetime', 'report')
     list_filter = ('user', 'level', 'datetime', 'report')
     ordering = ['-datetime']
+
 
 @admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
