ccef4c6b7eee4382c35795fb46f89d4c5420dc69
Jan Horacek <me@apophis.cz>
2019-02-22 22:41:28 +0100

    Avoid negative distance.

--- api_server/api_server/evaluator.py
+++ api_server/api_server/evaluator.py
@@ -59,6 +59,9 @@ def error_graph(level, stations, graph=None):
 
         nodes_dist = euclid_distance(graph['nodes'][nodea], graph['nodes'][nodeb])
         dista = euclid_distance(graph['nodes'][nodea], (x, y))
+
+        assert nodes_dist-dista >= 0
+
         s.append(eg.Station(nodea, nodeb, dista/nodes_dist, (nodes_dist-dista)/nodes_dist))
 
     g = eg.Graph(nodes, edges)
