b893b25802aa769b2c7acacbdce7a0c1a617d10a
Jan Horacek <me@apophis.cz>
2019-02-24 16:32:14 +0100

    Add svg to csv converter.

---
+++ solution/svg_to_csv.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+
+import sys
+import xml.etree.ElementTree
+import re
+import csv
+
+
+def main(ifn, ofn):
+    root = xml.etree.ElementTree.parse(ifn).getroot()
+
+    nodes = {}
+
+    for node in root.iter():
+        if node.tag.endswith('circle'):
+            match = re.search(r'fill:#(......)', node.attrib['style'])
+            color = match.group(1)
+            nodes[color] = nodes.get(color, []) + [(
+                (int(node.attrib['cx']), int(node.attrib['cy']))
+            )]
+
+    with open(ofn, 'w') as f:
+        writer = csv.writer(f, delimiter=',', quoting=csv.QUOTE_NONE)
+        for color, items in nodes.items():
+            for node in items:
+                writer.writerow(['node', node[0], node[1], color])
+
+
+if __name__ == '__main__':
+    if len(sys.argv) < 2:
+        sys.stderr.write('Usage: svg_to_csv.py input.svg output.csv\n')
+        sys.exit(1)
+
+    main(sys.argv[1], sys.argv[2])
